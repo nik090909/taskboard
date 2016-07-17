b096199326ff03a9419fcb87957b4a78d64868dd
nik <nik@grodno.net>
2016-07-17 19:37:23 +0300

    added authentication

---
+++ src/TaskBoard.Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaskBoard.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            string result = "You are not autorized";
+            if (User.Identity.IsAuthenticated)
+            {
+                result = $"Login: {User.Identity.Name}";
+            }
+            return result;
+        }
+    }
+}
---
+++ src/TaskBoard.Web/Models/ViewModels/RegisterModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskBoard.Web.Models.ViewModels
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Не указано имя пользователя")]
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Не указан пароль")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароль введен неверно")]
+        public string ConfirmPassword { get; set; }
+    }
+}
