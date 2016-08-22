d383ea28ad51156cdb241306573a29ae77f01ede
nik <nik@grodno.net>
2016-08-23 00:32:50 +0300

    #8 added a global exception filter class

--- src/TaskBoard.Web/App_Start/FilterConfig.cs
+++ src/TaskBoard.Web/App_Start/FilterConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TaskBoard.Web.Infrastructure.Attributes;
 
 namespace TaskBoard.Web
 {
@@ -11,6 +12,7 @@ namespace TaskBoard.Web
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new AuthorizeAttribute());
+            filters.Add(new GlobalExceptionFilter());
         }
     }
 }
---
+++ src/TaskBoard.Web/Infrastructure/Attributes/GlobalExceptionFilter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaskBoard.Web.Infrastructure.Attributes
+{
+    public class GlobalExceptionFilter : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext == null)
+            {
+                throw new ArgumentNullException("filterContext");
+            }
+
+            if (filterContext.ExceptionHandled || !filterContext.HttpContext.IsCustomErrorEnabled)
+            {
+                return;
+            }
+
+            Exception exception = filterContext.Exception;
+
+            if (!ExceptionType.IsInstanceOfType(exception))
+            {
+                return;
+            }
+
+            string controllerName = (string)filterContext.RouteData.Values["controller"];
+            string actionName = (string)filterContext.RouteData.Values["action"];
+            HandleErrorInfo model = new HandleErrorInfo(filterContext.Exception, controllerName, actionName);
+
+
+            filterContext.Result = new ViewResult
+            {
+                ViewName = View,
+                MasterName = Master,
+                ViewData = new ViewDataDictionary<HandleErrorInfo>(model),
+                TempData = filterContext.Controller.TempData
+            };
+
+            filterContext.ExceptionHandled = true;
+            filterContext.HttpContext.Response.Clear();
+            filterContext.HttpContext.Response.StatusCode = new HttpException(null, exception).GetHttpCode();
+
+            filterContext.HttpContext.Response.TrySkipIisCustomErrors = true;
+
+            //base.OnException(filterContext);
+        }
+    }
+}
